285c1392309f07ace07025f98ea625bc8655d017
Eric Curley <Eric.Curley94@gmail.com>
2020-04-11 00:03:28 -0400

    Move Player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public CharacterController controller;
+
+    public float speed = 12;
+    public float maxSpeed = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        x = x.Remap(0,1,0,z);
+
+        Debug.Log(x);
+
+        transform.Rotate(0f,x,0f);
+
+        
+        Vector3 move = new Vector3(0f,0f,0f);
+
+        if (z * speed > maxSpeed)
+        {
+            move = transform.forward * maxSpeed;
+        }else{
+            move = transform.forward * z;
+        }
+        
+        controller.Move(move * Time.deltaTime);
+    }
+
+}
